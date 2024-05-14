e431dbd282e9bfc91599d84212484988a028bb4a
pchalupa <chalupa.petr93@gmail.com>
2024-05-14 07:47:55 +0200

    docs: add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Coordinates } from './ExpoGeolocation.types';
 import ExpoGeolocationModule from './ExpoGeolocationModule';
 
+/**
+ * Requests permissions to access location data.
+ */
 export function requestPermissions(): void {
   ExpoGeolocationModule.requestPermissions();
 }
 
+/**
+ * Returns the current device's location.
+ */
 export async function getCurrentCoordinates(): Promise<Coordinates> {
   return ExpoGeolocationModule.getCurrentCoordinates();
 }
